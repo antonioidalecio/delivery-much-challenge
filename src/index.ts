ec132c47df3215f5e836d4e54d9267e757e2e400
Antonio Idalécio Pereira de Souza <antonio.souza@e-deploy.com.br>
2020-12-01 22:20:17 -0400

    moved express app instance to a separate file for a better integration with supertest

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import cors from 'cors'
 import dotenv from 'dotenv'
-import express from 'express'
 
-import routes from './routes'
+import server from './server'
 
 if (process.env.NODE_ENV === 'development') {
   dotenv.config()
@@ -10,12 +8,4 @@ if (process.env.NODE_ENV === 'development') {
 
 const port = process.env.APP_PORT || 3000
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-app.use(routes)
-
-app.listen(port, () => console.log(`listening on port ${port}`))
+server.listen(port, () => console.log(`Listening on port ${port}`))
