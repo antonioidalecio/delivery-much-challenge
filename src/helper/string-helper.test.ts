3fc978457cf8f8c24b99bce3ae548424d828a713
Antonio Idalécio Pereira de Souza <antonio.souza@e-deploy.com.br>
2020-12-04 00:38:42 -0400

    added unit tests for string helpers

---
+++ src/helper/string-helper.test.ts
@@ -0,0 +1,51 @@
+import {
+  commaDelimitedStringToStringArray,
+  stringArrayToCommaDelimitedString,
+} from './string-helper'
+
+describe('commaDelimitedStringToStringArray', () => {
+  it('should return empty array if provided string is empty', () => {
+    const emptyStrings = ['', '   ', '\t']
+
+    for (const value of emptyStrings) {
+      const result = commaDelimitedStringToStringArray(value)
+      expect(result).toEqual([])
+    }
+  })
+
+  it('should split values delimitted by comma and return corresponding list', () => {
+    const result = commaDelimitedStringToStringArray('a,b, c')
+    expect(result).toEqual(['a', 'b', 'c'])
+  })
+
+  it('should drop empty values when `filterEmptyValues` flag is provided as `true`', () => {
+    const result = commaDelimitedStringToStringArray('a,b,,c, ', {
+      filterEmptyValues: true,
+    })
+    expect(result).toEqual(['a', 'b', 'c'])
+  })
+
+  it('should keep empty values when `filterEmptyValues` flag is provided as `false`', () => {
+    const result = commaDelimitedStringToStringArray('a,b, ,c,', {
+      filterEmptyValues: false,
+    })
+    expect(result).toEqual(['a', 'b', '', 'c', ''])
+  })
+
+  it('should trim splited values', () => {
+    const result = commaDelimitedStringToStringArray('aa,bb  ,  c c')
+    expect(result).toEqual(['aa', 'bb', 'c c'])
+  })
+})
+
+describe('stringArrayToCommaDelimitedString', () => {
+  it('should return empty string if provided string array is empty', () => {
+    const result = stringArrayToCommaDelimitedString([])
+    expect(result).toEqual('')
+  })
+
+  it('should join values separated by comma', () => {
+    const result = stringArrayToCommaDelimitedString(['a', 'b ', ' c'])
+    expect(result).toEqual('a,b , c')
+  })
+})
